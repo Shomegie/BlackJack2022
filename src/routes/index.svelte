85c8cd67dad0ae90330c548b0aa50d72f67f7b52
Shomegie <shomegie@gmail.com>
2022-08-15 11:28:39 +0100

    completed hit gamestate

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,12 +12,15 @@ let player = true
 let card_index = 0
 let dealer_history =[]
 let player_history =[]
+let player_aceCount = 0
+let dealer_aceCount = 0
 let game_running = false
-let game_running2 = true
+let hit_stay_state = false
+
 
 let start_reset = true
 
-const ffanimate = () =>{
+const startGame = () =>{
         deck = shuffleDeck()
         start_reset = false
 
@@ -34,26 +37,61 @@ const ffanimate = () =>{
                         drawCard(dealer,"dealt")
                         render_card()
                         game_running = true
-
-                        // todo: hit/sit function
+                        hit_stay_state = true
                     },400)
                 },400)
             },400)
         },100)
+
 }
 
-let startGame = ()=>{
-        deck = shuffleDeck()
-        drawCard(player,"dealt")
-        //drawCard(dealer,"dealt")
-        render_card()
+const aceHandler = (final_total, aceCount) =>{
+    if (aceCount >= 1){
+        final_total += 10
 
-        drawCard(player,"dealt")
-        drawCard(dealer,"dealt")
-        render_card()
-        //hit_stay_state()
-        console.log(player_history)
+        if(final_total > 21)
+            final_total -= 10
+    }
+    return final_total
 }
+
+const hitAction = () =>{
+    drawCard(player,"hit")
+    render_card()
+    gameStatus()
+    if (player_total >21){
+        // hide hit/stay buttons
+        // player busts
+        // dealer reveals card
+        // end game
+        //exit_function()
+
+    } else if(player_total == 21){
+        // blackjack achieved
+        // replace hit/stay buttons with blackjack
+        // dealer reveals card
+        // dealer hits till 17
+
+        //exit_function()
+    }
+}
+
+let gameStatus = () =>{
+
+}
+
+// let startGame = ()=>{
+//         deck = shuffleDeck()
+//         drawCard(player,"dealt")
+//         //drawCard(dealer,"dealt")
+//         render_card()
+
+//         drawCard(player,"dealt")
+//         drawCard(dealer,"dealt")
+//         render_card()
+//         //hit_stay_state()
+//         console.log(player_history)
+// }
 
 
 
@@ -80,8 +118,12 @@ let drawCard = (user,action,facedown=false) => {
 
     if (user) {
         player_history.push({card: card, action: action, facedown:facedown, card_index:card_index})
+        if (card[0].cardvalue ==1) 
+            player_aceCount +=1
     }else{
         dealer_history.push({card: card, action: action, facedown:facedown, card_index:card_index})
+        if (card[0].cardvalue ==1) 
+            dealer_aceCount +=1
     }
 }
 
@@ -91,15 +133,6 @@ let render_card = ()=>{
 }
 
 
-
-// let cards = ["JC","7D","4C","AH"]
-let cards = ["JC","7D"]
-
-
-let dummy = () =>{
-    cards.push("JC")
-    cards = cards
-}
 
 $: preloadImageUrls = [...Array(52).keys()].map((key) => `/cards/${key+1}.svg`);
 
@@ -125,13 +158,13 @@ $: preloadImageUrls = [...Array(52).keys()].map((key) => `/cards/${key+1}.svg`);
                     <div class="h-2/5"></div>
                     <div class="h-3/5 relative">
                         {#if game_running }
-                            <div transition:fade class="bg-black absolute bottom-7 left-0 right-0 m-auto w-fit bg-opacity-10 text-opacity-60 text-teal-700 rounded-full text-xs py-1 px-4">{dealer_total}</div>
+                            <div transition:fade class="bg-black absolute bottom-7 xl:bottom-10 left-0 right-0 m-auto w-fit bg-opacity-10 text-opacity-60 text-teal-700 rounded-full text-xs py-1 px-4">{aceHandler(dealer_total,dealer_aceCount)}</div>
                         {/if}
                         <div class="absolute w-fit h-full left-0 right-0 m-auto flex mt-6 transition">
                             {#if dealer_history.length > 0}
                                 {#each dealer_history as image,i (i)}
                                     <img in:fly={{ x: 100, y:25, duration: 500 }}
-                                    out:fly={{ x: 100, duration: 400 }} src={`cards/${image.card[0].id}.svg`} class={`h-28 ${i==0?"":"-ml-14 mt-1"}`} alt={i}> 
+                                    out:fly={{ x: 100, duration: 400 }} src={`cards/${image.card[0].id}.svg`} class={`h-28 cursor-pointer ${i==0?"":"-ml-14 mt-1"}`} alt={i}> 
                                 {/each}
                             {:else}
                                 <p></p>
@@ -148,9 +181,9 @@ $: preloadImageUrls = [...Array(52).keys()].map((key) => `/cards/${key+1}.svg`);
             <div class="details w-full h-1/5 border-0 border-lime-600"></div>
             <div class="player w-full h-2/5 border-0 border-rose-600 p-2">
                 <div class="player_cards h-3/4 w-full flex items-center mx-auto relative ">
-                    {#if game_running}
+                    <!-- {#if game_running}
                     <div transition:fade class="bg-black absolute top-0 left-0 right-0 w-fit m-auto -mt-8 bg-opacity-10 text-opacity-70 text-teal-600 rounded-full text-xs py-1 px-4">Hand:{player_total}</div>
-                    {/if}
+                    {/if} -->
                     <div class="absolute left-0 right-0 m-auto mt-6 h-full w-fit drop flex transition-all">
                         {#if player_history.length > 0 }
                             {#each player_history as image,i (i)}
@@ -161,15 +194,28 @@ $: preloadImageUrls = [...Array(52).keys()].map((key) => `/cards/${key+1}.svg`);
                             <p></p>
                         {/if}
                     </div>
+                    {#if game_running}
+                        <div transition:fade class="bg-black absolute top-52 left-0 right-0 w-fit m-auto -mt-7 xl:-mt-2 bg-opacity-10 text-opacity-70 text-teal-600 rounded-full text-sm py-0 px-4">Hand:{aceHandler(player_total,player_aceCount)}</div>
+                    {/if}
                 </div>
+                {#if hit_stay_state}
+                    <div transition:fade class="hit_stay w-fit mx-auto flex  text-xl divide-teal-900 divide-x divide-opacity-20 pb-1 font-light text-opacity-60  text-teal-600">
+                        <button class="px-5 py-1 hover:cursor-pointer group -mt-0.5 group "><div class=" text-opacity-50 border-opacity-50 border-b pb-0 transition duration-200 group-hover:border-teal-500  group-hover:border-opacity-80 group-hover:text-teal-500  group-hover:text-opacity-80 border-teal-600 ">Stay</div></button>
+                        <button  class="px-6 py-1 tracking-wider cursor-pointer transition hover:text-teal-500 hover:text-opacity-90 duration-200 flex" on:click={hitAction}>Hit<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mt-1" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414l5-5a1 1 0 011.414 0l5 5a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414 0zm0-6a1 1 0 010-1.414l5-5a1 1 0 011.414 0l5 5a1 1 0 01-1.414 1.414L10 5.414 5.707 9.707a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                        </svg></button>
+                    </div>
+                {/if}
+
             </div>
+
         </div>
         <!-- Foreground: end -->
 
     <div class="controls text-indigo-200 absolute bottom-0 z-30 w-full p-4 flex justify-center mb-8">
         <div class="flex flex-col">
             {#if start_reset}
-                <div transition:fade class="play_button  cursor-pointer text-teal-700 p-2 px-4 flex justify-center" on:click={ffanimate}>Start</div>
+                <div transition:fade class="play_button cursor-pointer text-teal-700 p-2 px-4 flex justify-center" on:click={startGame}>Start</div>
             {/if}
         </div>
     </div>
